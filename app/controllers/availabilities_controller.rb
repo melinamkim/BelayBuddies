a78765af5092f2357bb8ff3d51cc2cc3c853c7dc
james hsia <jameshsia@hotmail.co.uk>
2022-06-07 14:43:52 +0100

    availability controller and view

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,2 +1,28 @@
 class AvailabilitiesController < ApplicationController
+
+  def new
+    @availability = Availability.new
+  end
+
+  def create
+    @availability = Availability.new(availability_params)
+    @availability.user = current_user
+    if @availability.save
+      redirect_to user_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  private
+
+  def availability_params
+    params.require(:availability).permit(:date, :begin_time, :end_time)
+	end
 end
