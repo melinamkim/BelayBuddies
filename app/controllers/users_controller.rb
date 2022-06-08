ed9c653477f491f9e0c90406bc1f496fd05a0d52
james hsia <jameshsia@hotmail.co.uk>
2022-06-08 13:16:17 +0100

    initial location search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
 
     @users = User.all
 
+    @users = @users.near(params[:location], 5) if params[:location].present? && params[:location] != ""
 
-
-    @users = @users.where("location = ?", params[:location]) if params[:gender].present? && params[:gender] != ""
+  # filters - to be done once geocoding aspect works
     @users = @users.where("gender = ?", params[:gender]) if params[:gender].present? && params[:gender] != ""
+    @users = @users.where("age = ?", params[:age]) if params[:age].present? && params[:age] != ""
+    @users = @users.where("level = ?", params[:level]) if params[:level].present? && params[:level] != ""
     @users = @users.where("type_of_climbing = ?", params[:type_of_climbing]) if params[:type_of_climbing].present? && params[:type_of_climbing] != ""
-    handle_search_location
-    handle_filters
+
   end
 
 
