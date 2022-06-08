dc134d7832ce3d46cd206f532e4348ad0661a6c8
Melina Kim <melinamkim@gmail.com>
2022-06-08 14:27:06 +0100

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@
 
 User.destroy_all
 
-User.create!(email: "melina@gmail.com", password: "123456", first_name: "Melina", last_name: "Kim", gender: "male", level: "beginner", age: 28, location: "Chelsea, London", indoor_bouldering:true )
+User.create!(email: "melina@gmail.com", password: "123456", first_name: "Melina", last_name: "Kim", gender: "female", level: "beginner", age: 28, location: "Canary Wharf, London", indoor_bouldering:true, indoor_sport_climbing:true)
 p "new user created"
-User.create!(email: "olivia@gmail.com", password: "123456", first_name: "Olivia", last_name: "Johnson", gender: "female", level: "intermediate", age: 22, location: "Kensington, London", indoor_bouldering:true )
+User.create!(email: "tassilo@gmail.com", password: "123456", first_name: "Tassilo", last_name: "Brecht", gender: "male", level: "beginner", age: 18, location: "Camden, London", indoor_bouldering:true)
 p "new user created"
-User.create!(email: "henry@gmail.com", password: "123456", first_name: "Henry", last_name: "Brown", gender: "male", level: "beginner", age: 33, location: "Greenwich, London", indoor_bouldering:true)
+User.create!(email: "james@gmail.com", password: "123456", first_name: "James", last_name: "Hsia", gender: "male", level: "intermediate", age: 50, location: "Finchley, London", outdoor_sport_climbing:true)
 p "new user created"
-User.create!(email: "lucas@gmail.com", password: "123456", first_name: "Lucas", last_name: "Jones", gender: "male", level: "advanced", age: 19, location: "Soho, London", indoor_bouldering:true, outdoor_sport_climbing:true )
+User.create!(email: "usman@gmail.com", password: "123456", first_name: "Usman", last_name: "Akram", gender: "male", level: "advanced", age: 35, location: "Dallston, London", indoor_bouldering:true, indoor_top_roping:true, outdoor_sport_climbing:true)
 p "new user created"
-User.create!(email: "mia@gmail.com", password: "123456", first_name: "Mia", last_name: "Davis", gender: "female", level: "advanced", age: 40, location: "Stratford, London", indoor_bouldering:true, outdoor_sport_climbing:true )
+User.create!(email: "phelim@gmail.com", password: "123456", first_name: "Phelim", last_name: "Dunleavy", gender: "male", level: "advanced", age: 28, location: "Hoxton, London", outdoor_trad_climbing:true, outdoor_sport_climbing:true)
 p "new user created"
-User.create!(email: "michael@gmail.com", password: "123456", first_name: "Michael", last_name: "Garcia", gender: "male", level: "intermediate", age: 25, location: "Hoxton, London", indoor_bouldering:true, outdoor_sport_climbing:true )
+User.create!(email: "louis@gmail.com", password: "123456", first_name: "Louis", last_name: "Leslie", gender: "male", level: "advanced intermediate", age: 25, location: "Hoxton, London", indoor_top_roping:true, outdoor_sport_climbing:true)
 p "new user created"
