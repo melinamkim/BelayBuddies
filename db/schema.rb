d0879634cc080f3d41b4dcbe3d5117f18a9dd26b
james hsia <jameshsia@hotmail.co.uk>
2022-06-08 12:04:29 +0100

    geocoding setup halfway

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_140936) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_105945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,15 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_140936) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "gender"
-    t.string "level"
-    t.integer "age"
-    t.string "location"
-    t.boolean "bouldering"
-    t.boolean "ice_climbing"
-    t.boolean "mountaineering"
-    t.boolean "alpine_climbing"
-    t.boolean "sport_climbing"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -73,6 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_140936) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
