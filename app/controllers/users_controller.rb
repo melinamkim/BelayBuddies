15a4c0c40ff2f09a4f894ced45bfebd7325585c6
james hsia <jameshsia@hotmail.co.uk>
2022-06-09 13:11:08 +0100

    basic filter working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   # Code may change dependent on the exact search functionality we want.
-  # i.e. initial search based on one criterion then further filtering VERSUS initial search using all filters.
   def index
     @users = User.all
 
@@ -8,9 +7,8 @@ class UsersController < ApplicationController
 
     # filters
     @users = @users.where("gender = ?", params[:gender]) if params[:gender].present? && params[:gender] != ""
-    @users = @users.where("age = ?", params[:age]) if params[:age].present? && params[:age] != ""
     @users = @users.where("level = ?", params[:level]) if params[:level].present? && params[:level] != ""
-    @users = @users.where("type_of_climbing = ?", params[:type_of_climbing]) if params[:type_of_climbing].present? && params[:type_of_climbing] != ""
+    @users = @users.where("types_of_climbing = ?", params[:type_of_climbing]) if params[:type_of_climbing].present? && params[:type_of_climbing] != ""
 
     @markers = @users.geocoded.map do |user|
       {
