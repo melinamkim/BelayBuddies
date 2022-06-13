5b5a92c3791c761559fd885cd0d969d258e35160
Melina Kim <melinamkim@gmail.com>
2022-06-13 17:57:40 +0100

    fix duplicated field in migration part 3

--- db/migrate/20220613092334_remove_and_add_columns_from_users.rb
+++ db/migrate/20220613092334_remove_and_add_columns_from_users.rb
@@ -3,8 +3,6 @@ class RemoveAndAddColumnsFromUsers < ActiveRecord::Migration[7.0]
     remove_column :users, :bouldering
     remove_column :users, :sport_climbing
     remove_column :users, :top_roping
-    remove_column :users, :bouldering
-    remove_column :users, :sport_climbing
     remove_column :users, :trad_climbing
     add_column :users, :bouldering, :boolean, null: false, default: true
     add_column :users, :sport_climbing, :boolean, null: false, default: true
