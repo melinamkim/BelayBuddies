1716de7efadf828082bc71e88951517f3ee619df
Melina Kim <melinamkim@gmail.com>
2022-06-15 17:33:14 +0100

    Add stimulus styling for live incoming chat

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -7,15 +7,31 @@ export default class extends Controller {
 
   connect() {
     console.log('HELLO');
+    console.log(this.element.dataset.user)
     this.channel = createConsumer().subscriptions.create(
       { channel: 'ChatroomChannel', id: this.chatroomIdValue },
       {
         received: (message) => {
           console.log('HELLO')
+          console.log(message)
           this.messagesTarget.insertAdjacentHTML('beforeend', message)
+          if (this.messagesTarget.lastElementChild.dataset.user == this.element.dataset.user) {
+
+            console.log("Same User")
+            this.messagesTarget.lastElementChild.classList.add("justify-content-end")
+            this.messagesTarget.lastElementChild.firstElementChild.classList.add("my-message")
+            this.messagesTarget.lastElementChild.classList.remove("justify-content-start")
+          } else {
+            console.log("Other User")
+            this.messagesTarget.lastElementChild.classList.add("justify-content-start")
+            this.messagesTarget.lastElementChild.firstElementChild.classList.add("their-message")
+            this.messagesTarget.lastElementChild.classList.remove("justify-content-end")
+          }
           this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
         }
      }
     )
   }
+
+
 }
